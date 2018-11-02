67841f74e9b8cc8211ee47c862dc8a3bedf089b9
inux <inux.steve@gmail.com>
2018-11-02 16:53:55 +0100

    added more folders

--- bkp.sh
+++ bkp.sh
@@ -8,16 +8,20 @@ DISKSTATION=/Volumes/diskstation/bkp/
 directories=(
     '/Users/inux/Projects/'
     '/Users/inux/Documents/'
+    '/Users/inux/Pictures/'
+    '/Users/inux/Steuerfälle/'
+    '/Users/inux/go/src/hoau/'
+    '/Users/inux/go/src/steveineichenWeb/'
 )
 
-#makes the actual bakup
+#do the actual bakup
 doBackup () {
     echo $DISKSTATION' is available, starting bkp...'
     for d in "${directories[@]}"
     do
         echo 'Backup: '$d
         mkdir -p $DISKSTATION$d
-        rsync -avzr --rsh="ssh -c arcfour" --delete-after --whole-file --info=progress2 $d $DISKSTATION$d
+        rsync -avzr --rsh="ssh -c arcfour" --delete-after --whole-file --info=progress2 --exclude .git/ --exclude node_modules/ $d $DISKSTATION$d
     done
     echo $DISKSTATION' Done. Successful'
 }
@@ -25,7 +29,7 @@ doBackup () {
 #check if backup is possible
 if [ -d "$DISKSTATION" ]; then
     time doBackup
-else 
+else
     echo $DISKSTATION' is not available. Nothing to do...'
 fi
 
